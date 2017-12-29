dc9d5a1425fd98e9e6ec12ecfa0f319fc571d36e
kbellioum <kbellioum@gmail.com>
2017-12-29 10:45:25 +0000

    menu publication

--- src/components/Publication.vue
+++ src/components/Publication.vue
@@ -4,7 +4,7 @@
     <!-- <h1>Menu Publication</h1> -->
     <!-- <hr> -->
     <!-- <img src="http://via.placeholder.com/1024x500?text=Publication" alt=""> -->
-    <div class="" id="gallery">
+    <!-- <div class="" id="gallery">
       <div class="view">
         <a href="../static/assets/publications/alaakar.pdf">
         <pdf src="../static/assets/publications/alaakar.pdf"></pdf>
@@ -15,6 +15,12 @@
         <pdf src="../static/assets/publications/architecturemaroc.pdf"></pdf>
         </a>
       </div>
+    </div> -->
+
+    <div id="gallery" style="display:none;">
+      <a v-for="item in list" :href="item.link">
+        <img  alt="Image 1 Title" :src=item.source>
+      </a>
     </div>
 
 
@@ -27,7 +33,18 @@ export default {
   name: 'Publication',
   data () {
     return {
-      msg: ''
+      msg: '',
+      list: [
+        {id: '1', source: '../static/assets/publications/architecturemaroc.png', link: '../static/assets/publications/architecturemaroc.pdf'},
+        {id: '2', source: '../static/assets/publications/alaakar.png', link: '../static/assets/publications/alaakar.pdf'},
+        {id: '3', source: '../static/assets/publications/installmaroc.png', link: '../static/assets/publications/installmaroc.pdf'},
+        {id: '3', source: '../static/assets/publications/maison_du_maroc.png', link: '../static/assets/publications/maison_du_maroc.pdf'},
+        {id: '3', source: '../static/assets/publications/premium.png', link: '../static/assets/publications/premium.pdf'},
+        {id: '3', source: '../static/assets/publications/prestige.png', link: '../static/assets/publications/prestige.pdf'},
+        {id: '3', source: '../static/assets/publications/deco_actual.png', link: '../static/assets/publications/deco_actual.pdf'},
+        {id: '3', source: '../static/assets/publications/brandup.png', link: '../static/assets/publications/brandup.pdf'}
+
+      ]
     }
   },
   components: {
@@ -38,8 +55,11 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.view {
+/* .view {
   display: inline-block;
   width: 400px;
+} */
+body {
+  overflow: visible !important;
 }
 </style>
