bb436c438beb6648b6e256a6626bc091464bb609
kbellioum <kbellioum@gmail.com>
2017-12-29 20:31:18 +0000

    OK

--- src/components/Projets.vue
+++ src/components/Projets.vue
@@ -9,44 +9,7 @@ export default {
   name: 'Projets',
   data () {
     return {
-      msg: '',
-      list: [
-        {id: '1', source: '../static/assets/homeslider/images/big/img-slider-1.jpg'},
-        {id: '2', source: '../static/assets/homeslider/images/big/img-slider-2.jpg'},
-        {id: '3', source: '../static/assets/homeslider/images/big/img-slider-3.jpg'},
-        {id: '4', source: '../static/assets/homeslider/images/big/img-slider-4.jpg'},
-        {id: '5', source: '../static/assets/homeslider/images/big/img-slider-5.jpg'},
-        {id: '1', source: '../static/assets/homeslider/images/big/img-slider-1.jpg'},
-        {id: '2', source: '../static/assets/homeslider/images/big/img-slider-2.jpg'},
-        {id: '3', source: '../static/assets/homeslider/images/big/img-slider-3.jpg'},
-        {id: '4', source: '../static/assets/homeslider/images/big/img-slider-4.jpg'},
-        {id: '5', source: '../static/assets/homeslider/images/big/img-slider-5.jpg'},
-        {id: '1', source: '../static/assets/homeslider/images/big/img-slider-1.jpg'},
-        {id: '2', source: '../static/assets/homeslider/images/big/img-slider-2.jpg'},
-        {id: '3', source: '../static/assets/homeslider/images/big/img-slider-3.jpg'},
-        {id: '4', source: '../static/assets/homeslider/images/big/img-slider-4.jpg'},
-        {id: '5', source: '../static/assets/homeslider/images/big/img-slider-5.jpg'},
-        {id: '1', source: '../static/assets/homeslider/images/big/img-slider-1.jpg'},
-        {id: '2', source: '../static/assets/homeslider/images/big/img-slider-2.jpg'},
-        {id: '3', source: '../static/assets/homeslider/images/big/img-slider-3.jpg'},
-        {id: '4', source: '../static/assets/homeslider/images/big/img-slider-4.jpg'},
-        {id: '5', source: '../static/assets/homeslider/images/big/img-slider-5.jpg'},
-        {id: '1', source: '../static/assets/homeslider/images/big/img-slider-1.jpg'},
-        {id: '2', source: '../static/assets/homeslider/images/big/img-slider-2.jpg'},
-        {id: '3', source: '../static/assets/homeslider/images/big/img-slider-3.jpg'},
-        {id: '4', source: '../static/assets/homeslider/images/big/img-slider-4.jpg'},
-        {id: '5', source: '../static/assets/homeslider/images/big/img-slider-5.jpg'},
-        {id: '1', source: '../static/assets/homeslider/images/big/img-slider-1.jpg'},
-        {id: '2', source: '../static/assets/homeslider/images/big/img-slider-2.jpg'},
-        {id: '3', source: '../static/assets/homeslider/images/big/img-slider-3.jpg'},
-        {id: '4', source: '../static/assets/homeslider/images/big/img-slider-4.jpg'},
-        {id: '5', source: '../static/assets/homeslider/images/big/img-slider-5.jpg'}
-      ]
-    }
-  },
-  methods: {
-    doit () {
-      console.log('OK OK')
+      msg: ''
     }
   }
 }
