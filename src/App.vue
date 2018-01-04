ebe38a7191294e1537765e8df2f2506fa3fcbf05
kbellioum <kbellioum@gmail.com>
2018-01-04 09:12:35 +0000

    OK scroll marquee with the speed 5

--- src/App.vue
+++ src/App.vue
@@ -90,7 +90,7 @@
               <span class="newsalign">News</span>
             </div>
             <div class="newsinfo">
-              <marquee class="infoalign" behavior="scroll" direction="right" width="400px">{{ infos }}</marquee>
+              <marquee class="infoalign" scrollamount="5" behavior="scroll" direction="right" width="400px">{{ infos }}</marquee>
             </div>
             <div class="youtube">
               <a href="#"><i class="fa fa-youtube" aria-hidden="true"></i></a>
@@ -128,7 +128,7 @@ export default {
   name: 'app',
   data () {
     return {
-      infos: 'Test Information'
+      infos: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
     }
   },
   methods: {
