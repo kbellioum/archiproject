b708f4aab865f5210dd782e468fafdee608dc538
kbellioum <kbellioum@gmail.com>
2018-03-16 00:28:21 +0000

    HomeProjet Cleanup

--- src/components/HomeProjet.vue
+++ src/components/HomeProjet.vue
@@ -19,7 +19,7 @@
 
 <script>
 // import { sendmail } from '../lib/index.js'
-import axios from 'axios'
+// import axios from 'axios'
 export default {
   name: 'HomeProjet',
   data () {
@@ -35,36 +35,36 @@ export default {
     }
   },
   created () {
-    axios({
-      url: 'https://api.mailgun.net/v3/sandbox7fe163bd9a5b494c93fc6bdef403637f.mailgun.org/',
-      method: 'POST',
-      dataType: 'json',
-      auth: {
-        username: 'api',
-        password: 'key-0720e90820d91b543d01e5c84989d583'
-      },
-      data: {
-        'from': 'karim@sandbox7fe163bd9a5b494c93fc6bdef403637f.mailgun.org',
-        'to': 'kbellioum@gmail.com'
-      },
-      params: {
-        // https://api.rss2json.com/v1/api.json?rss_url=http%3A%2F%2Fwww.archimedia.ma%2Findex.php%3Fformat%3Dfeed%26type%3Drss&api_key=wl6vfhwive3beojsb3arqjxlmtg8fkevdnlmfjui
-        // rss_url: 'http://www.archimedia.ma/index.php?format=feed&type=rss',
-        // api_key: 'wl6vfhwive3beojsb3arqjxlmtg8fkevdnlmfjui',
-        // count: 5
-      }
-    })
-    .then(response => {
-      console.log('response Allo', response)
-      // console.log('response data', response.data)
-      // console.log('====== ' + response.feed.title + ' ======')
-      // this.rss_title = response.data.items
-      // response.data.items[Math.floor(Math.random() * response.data.items.length)].title
-      // this.infos = response.data.feed.description
-    })
-    .catch(e => {
-      this.errors.push(e)
-    })
+    // axios({
+    //   url: 'https://api.mailgun.net/v3/sandbox7fe163bd9a5b494c93fc6bdef403637f.mailgun.org/',
+    //   method: 'POST',
+    //   dataType: 'json',
+    //   auth: {
+    //     username: 'api',
+    //     password: 'key-0720e90820d91b543d01e5c84989d583'
+    //   },
+    //   data: {
+    //     'from': 'karim@sandbox7fe163bd9a5b494c93fc6bdef403637f.mailgun.org',
+    //     'to': 'kbellioum@gmail.com'
+    //   },
+    //   params: {
+    //     // https://api.rss2json.com/v1/api.json?rss_url=http%3A%2F%2Fwww.archimedia.ma%2Findex.php%3Fformat%3Dfeed%26type%3Drss&api_key=wl6vfhwive3beojsb3arqjxlmtg8fkevdnlmfjui
+    //     // rss_url: 'http://www.archimedia.ma/index.php?format=feed&type=rss',
+    //     // api_key: 'wl6vfhwive3beojsb3arqjxlmtg8fkevdnlmfjui',
+    //     // count: 5
+    //   }
+    // })
+    // .then(response => {
+    //   console.log('response Allo', response)
+    //   // console.log('response data', response.data)
+    //   // console.log('====== ' + response.feed.title + ' ======')
+    //   // this.rss_title = response.data.items
+    //   // response.data.items[Math.floor(Math.random() * response.data.items.length)].title
+    //   // this.infos = response.data.feed.description
+    // })
+    // .catch(e => {
+    //   this.errors.push(e)
+    // })
     // console.log('test')
     // sendmail.terra()
   },
